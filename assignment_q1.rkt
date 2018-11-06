45c263c37e31b3c64ec0efe8dae378edf008ced0
BRIAN  KENNY <16397953@IT.NUIGALWAY.IE>
2018-11-06 18:58:18 +0000

    question1 answered

--- assignment_q1.rkt
+++ assignment_q1.rkt
@@ -1,1 +1,6 @@
 #lang racket
+(cons 1 2)
+(cons 452 (cons 6  (cons 13 empty)))
+(cons "string"(cons 32 (cons (cons 452 (cons 6  (cons 13 empty))) empty)))
+(list "String" 32 (list 1 5 78))
+(append '("String") '(344) '( (4 5 7)))
