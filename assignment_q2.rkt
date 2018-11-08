f0a062b03dab49eb69a9f054a0bc091c1614dcff
BRIAN  KENNY <16397953@it.nuigalway.ie>
2018-11-08 16:36:54 +0000

    Question 2 answered

--- assignment_q2.rkt
+++ assignment_q2.rkt
@@ -12,6 +12,53 @@
 ;You may delete these comments!
 
 (provide ins_beg)
+(provide ins_end)
+(provide cout_top_level)
+(provide count_instances)
+(provide count_instances_tr)
+(provide tail)
+(provide count_instances_deep)
 
 (define (ins_beg el lst)
-  (display "Hello, I'm ins_beg!\n"))
+  (append (list el) lst )
+)
+
+(define (ins_end el lst)
+  (append lst (list el))
+)
+
+(define (cout_top_level lst)
+   (length lst))
+
+(define (count_instances el lst)
+(cond
+        [(null? lst) 0]
+	[ ( eq? el (car lst)) (+ 1 (count_instances el (cdr lst)))]
+        [else (count_instances el (cdr lst)) ]
+        )
+)
+
+
+(define (tail el lst)
+  (count_instances_tr el lst 0))
+
+(define (count_instances_tr el lst num)
+ (cond
+        [(null? lst) num]
+	[ ( eq? el (car lst)) (count_instances_tr el (cdr lst) (+ 1 num))]
+        [else (count_instances_tr el (cdr lst) num) ]
+        )
+ 
+  )
+
+
+(define (count_instances_deep el lst)
+  (cond
+        [(null? lst) 0]
+	[ ( eq? el (car lst)) (+ 1 (count_instances_deep el (cdr lst)))]
+        [(list? (car lst))
+         (+ 0 (count_instances_deep el (cdr lst))
+            (count_instances_deep el (car lst)))]
+        [else (count_instances_deep el (cdr lst)) ]
+        )
+  )
--- tests.rkt
+++ tests.rkt
@@ -14,13 +14,34 @@
   (begin
     (display "Running tests...\n")
     ;begin calling test functions
-    (printf "1: ~a" (test_ins_beg1))
+    (printf "a: ~a\n" (test_ins_beg1))
+    (printf "b: ~a\n" (test_ins_end1))
+    (printf "c: ~a\n" (test_ins_cout))
+    (printf "d: ~a\n" (test_ins_cinst))
+    (printf "e: ~a\n" (test_ins_ctail))
+    (printf "f: ~a\n" (test_ins_cdeep))
     ;end calling test functions
     (display "\nTests complete!\n")))
 
 ;Begin test functions
 (define (test_ins_beg1)
-  (eq? (ins_beg 1 '(2 3 4)) '(1 2 3 4)))
+  (equal? (ins_beg 1 '(2 3 4)) '(1 2 3 4)))
+
+(define (test_ins_end1)
+  (equal? (ins_end 1 '(2 3 4)) '(2 3 4 1)))
+
+(define (test_ins_cout)
+  (eq? (cout_top_level '(1 5 4 '(2 3) '(2 3 4))) 5))
+
+(define (test_ins_cinst)
+  (eq? (count_instances 2 '(1 5 2 4 2 7 2)) 3))
+
+(define (test_ins_ctail)
+  (eq? (tail 2 '(1 5 2 4 2 7 2)) 3))
+
+(define (test_ins_cdeep)
+  (eq? (count_instances_deep 2 '(1 5 2 4 '(2 7) 2)) 3))
+
 
 ;End test functions
 
